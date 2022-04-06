619b1455796fca50a2aa417e3ce3f6b36aa2485c
NghiNDP <ng.d.phuongnghi@gmail.com>
2022-04-06 10:39:31 +0700

    import request package for access the uri of fb

--- src/controllers/chatboxController.js
+++ src/controllers/chatboxController.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
@@ -78,7 +79,7 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
       "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
       "method": "POST",
       "json": request_body
     }, (err, res, body) => {
@@ -128,7 +129,6 @@ let postWebHook = (req, res) => {
 
 let getWebHook = (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse the query params
   let mode = req.query["hub.mode"];
